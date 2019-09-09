e248722d8fc98acb4e1a90cab1a6013dea6df2d2
Li Xie <li_xie1992@hotmail.com>
2019-09-09 03:54:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = { yolo: #{value} }
+  hash = { yolo: value }
 end
 
 def read_from_hash(hash, key)
